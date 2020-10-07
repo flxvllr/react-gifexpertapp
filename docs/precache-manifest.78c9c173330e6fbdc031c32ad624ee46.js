c1ac4357d662584823657b60af743a263adb8791
fvillar <flxvllr@gmail.com>
2020-10-07 20:49:27 +0200

    docs agregado

---
+++ docs/precache-manifest.78c9c173330e6fbdc031c32ad624ee46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3911abe0bdf4c897ed9622acfd6a0fe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c1eca18a014442e08a3",
+    "url": "/static/css/main.6c2c56c9.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c1eca18a014442e08a3",
+    "url": "/static/js/main.3c3358e6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
